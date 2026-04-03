468fed9c95604f8e5abccb264b55513e3050de49
Pulkit Luharuka <pulkit@Pulkits-MacBook-Air.local>
2026-04-04 01:24:35 +0530

    Pulkit's new file added

---
+++ idk.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef pair<int,int> pi;
+#define F first;
+#define S second;
+#define PB push_back;
+#define len(s) (int)s.size;
+#define REP(i,a,b) for(int i=a;i <= b;i++)
+#define endl '\n'
+#define fast ios_base::sync_with_stdio(0);cin.tie(0);cout.tie(0);
+ll mod=1000000007;
+int main(){
+   fast;
+   ll t;
+   cin>> t;
+   while(t--){
+        cout<<"~Pulkit"<<endl;
+   }
+
+   return 0;
+}
+
+
+
+
